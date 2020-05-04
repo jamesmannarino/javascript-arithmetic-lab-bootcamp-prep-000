15214eb11243b9e20f4ed26fda4b00bb8adbc263
James Mannarino <jamesmannarino@gmail.com>
2020-05-04 22:10:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function add() {
+function add(a, b) {
   return 2+2
 }
 
-function subtract() {
+function subtract(a, b) {
   return 4-2
 }
 
-function multiply() {
+function multiply(a, b) {
   return 5 * 6
 }
 
-function divide() {
+function divide(a, b) {
   return 6/3
 }
 

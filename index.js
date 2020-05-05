7723fdb22360a8e2e2346700e31641bcef823b4b
James Mannarino <jamesmannarino@gmail.com>
2020-05-05 16:02:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ function decrement(n) {
 }
 
 function makeInt (n) {
-  parseInt('n',10)
+  parseInt(n,10)
   return n
 }
 
 function preserveDecimal(n) {
-  parseFloat('n')
+  parseFloat(n)
   return n
 }
 
